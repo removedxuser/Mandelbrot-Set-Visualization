8d21e85bdf8fa18e00609a07b2e320da952bfcd1
Amogh <amrcdrc@gmail.com>
2020-05-01 09:27:47 +0545

    added initial frame

---
+++ FractalExplorer.java
@@ -0,0 +1,30 @@
+/** Mandelbrot Fractal Visulaizer by Amogh Rijal 2020 May **/
+
+import java.util.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FractalExplorer extends JFrame {
+
+  static final int WIDTH = 600;
+  static final int HEIGHT = 600;
+
+  public FractalExplorer() {
+    setInitialGUIProperties();
+  }
+
+  public void setInitialGUIProperties() {
+    this.setTitle("Fractal Explorer");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(WIDTH, HEIGHT);
+    this.setResizable(false);
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+  }
+
+  public static void main (String[] args) {
+    new FractalExplorer();
+  }
+}
